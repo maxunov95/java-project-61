9d08883cfb75456615d563a0c36de774b786bd34
maxunov95 <maxunov95@gmail.com>
2023-08-06 13:07:21 +0400

    step 3

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void greet() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        System.out.println("Hello, " + scanner.next() + "!");
+
+        scanner.close();
+    }
+}
