71823f44d249a0f8c1c3de0367028baac3032d85
maxunov95 <maxunov95@gmail.com>
2023-09-03 15:13:46 +0400

    corrected according to comments

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class Engine {
     public static final int MAX_COUNT_ROUNDS = 3;
-    public static final int INDEX_ROUND_QUESTION = 0;
-    public static final int INDEX_ROUND_ANSWER = 1;
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void run(String rules, String[][] rounds) {
@@ -16,17 +14,24 @@ public class Engine {
             System.out.printf("Question: %s%nYour answer: ", rounds[i][0]);
 
             String userAnswer = SCANNER.next();
-            if (rounds[i][1].equals(userAnswer)) {
-                System.out.println("Correct!");
-            } else {
-                System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.%nLet's try again, %s!%n",
-                        userAnswer, rounds[i][1], userName);
+            if (!checkAnswer(rounds, i, userAnswer, userName)) {
                 break;
             }
 
             if (i == MAX_COUNT_ROUNDS - 1) {
                 System.out.printf("Congratulations, %s!%n", userName);
             }
+        }
+    }
+
+    private static boolean checkAnswer(String[][] rounds, int i, String userAnswer, String userName) {
+        if (rounds[i][1].equals(userAnswer)) {
+            System.out.println("Correct!");
+            return false;
+        } else {
+            System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.%nLet's try again, %s!%n",
+                    userAnswer, rounds[i][1], userName);
+            return true;
         }
     }
 
